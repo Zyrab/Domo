4a1a28ec0043137e25030222478a258061455779
zyrab <zurakrua@gmail.com>
2025-04-21 21:27:54 +0200

    docs: added types for jsDoc in index.js and DSVG export with support for .class

--- index.js
+++ index.js
@@ -1,4 +1,35 @@
-import Domo from "./src/core/Domo.js";
-import Router from "./src/core/Router.js";
+// @ts-check
 
-export { Domo, Router };
+import DomoClass from "./src/core/Domo.js";
+import { Router } from "./src/core/Router.js";
+import DomoSVGClass from "./src/core/DomoSVG.js";
+
+/**
+ * @typedef {import('./src/core/Domo.js').default} DomoInstance
+ * @typedef {import('./src/core/DomoSVG.js').default} DomoSVGInstance
+ */
+
+/**
+ * @typedef {((tag?: string) => DomoInstance) & { Class: typeof DomoClass }} DomoFactory
+ * @typedef {((tag?: string) => DomoSVGInstance)} DomoSVGFactory
+ */
+
+/**
+ * @param {string} [tag]
+ * @returns {DomoInstance}
+ */
+function Domo(tag) {
+  return new DomoClass(tag);
+}
+Domo.Class = DomoClass;
+
+/**
+ * @param {string} [tag]
+ * @returns {DomoSVGInstance}
+ */
+function DSVG(tag) {
+  return new DomoSVGClass(tag);
+}
+DSVG.Class = DomoSVGClass;
+
+export { Domo, Router, DSVG, DomoClass };
