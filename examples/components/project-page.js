2e32d2587c1b8b357384daf7257b74837a960387
zyrab <zurakrua@gmail.com>
2025-09-26 15:03:54 +0200

    ref(examples): refactored projects pages to work with outlets

--- examples/components/project-page.js
+++ examples/components/project-page.js
@@ -4,8 +4,9 @@ import { loadJson } from "../load-json.js";
 
 export default async function createProjectPage(params) {
   const data = (await loadJson(`/dist/data/${params.id}.json`)) || {};
-  const testPAges = ["run", "go", "why", "because"];
-  return Domo()
+  const testPages = ["run", "go", "why", "because"];
+
+  return Domo("section")
     .on(
       "click",
       (e) => {
@@ -16,48 +17,70 @@ export default async function createProjectPage(params) {
       },
       { ssg: false }
     )
-    .css({
-      maxWidth: "800px",
-      margin: "40px auto",
-      padding: "24px",
-      border: "1px solid #ddd",
-      borderRadius: "8px",
-      fontFamily: "Arial, sans-serif",
-      backgroundColor: "#fff",
-      boxShadow: "0 2px 8px rgba(0,0,0,0.05)",
-    })
+    .css(styles.section)
     .child([
-      Domo("h1").txt(data?.title).css({
-        fontSize: "28px",
-        marginBottom: "16px",
-        color: "#333",
-      }),
-      Domo("p").txt(data?.description).css({
-        fontSize: "16px",
-        color: "#666",
-        marginBottom: "20px",
-      }),
-      Domo("div").txt(data?.content).css({
-        fontSize: "15px",
-        lineHeight: "1.6",
-        color: "#444",
-      }),
-      testPAges.map((page) =>
-        Domo("a")
-          .css({
-            display: "felx",
-            gap: "0.5rem",
-            padding: "0.8rem",
-          })
-          .cls("nav-link")
-          .data({ link: page })
-          .attr({ href: `/projects/${params.id}/${page}` })
-          .txt("test page")
-      ),
-      Domo("div").txt(`Last updated: ${data?.updated}`).css({
-        marginTop: "30px",
-        fontSize: "13px",
-        color: "#999",
-      }),
+      Domo()
+        .css(styles.container)
+        .child([
+          Domo("h1").txt(data?.title).css(styles.title),
+          Domo("p").txt(data?.description).css(styles.description),
+          Domo("div").txt(data?.content).css(styles.content),
+          Domo().child(
+            testPages.map((page) =>
+              Domo("a")
+                .cls("nav-link")
+                .data({ link: page })
+                .attr({ href: `/projects/${params.id}/${page}` })
+                .css(styles.navLink)
+                .txt(`Test ${page}`)
+            )
+          ),
+          Domo("div").txt(`Last updated: ${data?.updated}`).css(styles.updated),
+        ]),
+      params?.outlet ? params.outlet(params) : "",
     ]);
 }
+
+const styles = {
+  section: {
+    display: "flex",
+    flexDirection: "column",
+    gap: "3rem",
+    alignItems: "center",
+    padding: "3rem",
+  },
+  container: {
+    maxWidth: "800px",
+    padding: "2rem",
+    border: "1px solid #ddd",
+    borderRadius: "0.5rem",
+    display: "flex",
+    flexDirection: "column",
+    gap: "0.5rem",
+    fontFamily: "Arial, sans-serif",
+    backgroundColor: "#fff",
+    boxShadow: "0 2px 8px rgba(0,0,0,0.05)",
+  },
+  title: {
+    fontSize: "2rem",
+    color: "#333",
+  },
+  description: {
+    fontSize: "1.25rem",
+    color: "#666",
+  },
+  content: {
+    fontSize: "1.2rem",
+    lineHeight: "1.6",
+    color: "#444",
+  },
+  navLink: {
+    padding: "0.8rem",
+    cursor: "pointer",
+    textDecoration: "none",
+  },
+  updated: {
+    fontSize: "0.8rem",
+    color: "#999",
+  },
+};
--- examples/components/testing-page.js
+++ examples/components/testing-page.js
@@ -2,6 +2,7 @@ import Domo from "../../packages/domo/src/domo.js";
 
 export default function testPage(props) {
   const propKeys = Object.keys(props);
+  // console.log(props);
 
   return Domo()
     .txt("testing page")
