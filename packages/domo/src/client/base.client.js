ec4b113bd1dd33d5470185f74fb2b4ef64356c4f
zyrab <zurakrua@gmail.com>
2026-03-19 19:03:08 +0100

    feat(spa): domo client base  clases and ref method

---
+++ packages/domo/src/client/base.client.js
@@ -0,0 +1,35 @@
+/**
+ * @class BaseClient
+ * @description Foundational class for Client-side Domo elements.
+ */
+class BaseClient {
+  /**
+   * @property {HTMLElement} element - The actual DOM element.
+   */
+  element;
+
+  /**
+   * @property {boolean} _isDomo - Flag to identify Domo instances.
+   */
+  _isDomo = true;
+
+  /**
+   * Creates an instance of BaseClient.
+   * @param {string} [el="div"] - The HTML tag name.
+   */
+  constructor(el = "div") {
+    this.element = document.createElement(String(el || "div").toLowerCase());
+  }
+
+  /**
+   * In browser context, passes the actual DOM element to the callback.
+   * @param {function(HTMLElement): void} callback
+   * @returns {this}
+   */
+  ref(callback) {
+    if (typeof callback === "function") callback(this.element);
+    return this;
+  }
+}
+
+export default BaseClient;
